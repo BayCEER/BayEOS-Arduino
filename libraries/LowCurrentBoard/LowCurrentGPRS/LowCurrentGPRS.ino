b9e7fc967262c4cd8722c05c741fbf9658d2cc9b
Stefan Holzheu <holzheu@bayceer.uni-bayreuth.de>
2016-03-06 11:15:54 +0100

    + improvements LowCurrentGPRS.ino

--- libraries/LowCurrentBoard/LowCurrentGPRS/LowCurrentGPRS.ino
+++ libraries/LowCurrentBoard/LowCurrentGPRS/LowCurrentGPRS.ino
@@ -43,33 +43,36 @@ void setup()
   digitalWrite(GPRS_POWER_PIN,LOW);
   initLCB();
   //CHANGE CONFIG!!
-  client.readConfigFromStringPGM(PSTR("132.180.112.55|80|gateway/frame/saveFlat|import|import|LP-GPRS2|internet|||1802|"));
+  client.readConfigFromStringPGM(PSTR("192.168.2.1|80|gateway/frame/saveFlat|import|import|LP-GPRS|web.vodafone.de|||1234|"));
   blinkLED(2); 
   client.softSwitch();
   res=client.begin(38400);
   blinkLED(res+1); 
   /*
-         * 1 == OK
-	 * 2 == NO Communication
-	 * 3 == PIN failed
-	 * 4 == PIN locked
-	 * 5 == Not CREG
-	 * 6 == Not CGATT
-	 * 7 == No SIM Card
+   * 1 == OK
+   * 2 == NO Communication
+   * 3 == PIN failed
+   * 4 == PIN locked
+   * 5 == Not CREG
+   * 6 == Not CGATT
+   * 7 == No SIM Card
   */
   delay(2000);
   
   res=client.sendMessage("GPRS started");
   blinkLED(res+1);
+  
   /*
-         * 1 == OK
-   	 * 2 == no success
-	 * 3 == timeout
-	 * 4 == network timeout
-	 * 5 == gprs modem timeout
+   * 1 == OK
+   * 2 == no success
+   * 3 == timeout
+   * 4 == network timeout
+   * 5 == gprs modem timeout
   */ 
   
   digitalWrite(GPRS_POWER_PIN,HIGH);
+  Serial.end();
+  pinMode(1,INPUT);
  
   Wire.begin();
   myBuffer.init(0x50,65536L,0); //NO flush!!
@@ -78,7 +81,7 @@ void setup()
   initLCB(); //init time2   
   readBatLCB(); 
   startLCB();
-  gprs_counter=GPRS_SEND_COUNT-2;
+  gprs_counter=GPRS_SEND_COUNT-1;
 }
 
 
@@ -103,14 +106,17 @@ void loop()
         client.begin(38400);
         gprs_status=1;
       }
-      if(client.sendMultiFromBuffer()) tx_error++;
+      if(res=client.sendMultiFromBuffer(2000)) tx_error++;
       else tx_error=0;
+      blinkLED(res+1);
       
-      if(tx_error>2 || ! myBuffer.available()){
+      if(tx_error>5 || ! myBuffer.available()){
         gprs_counter=0;
         gprs_status=0;
         tx_error=0;
         digitalWrite(GPRS_POWER_PIN,HIGH);
+        Serial.end();
+        pinMode(1,INPUT);
       }
     }
   }
